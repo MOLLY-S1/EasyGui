1dc6a1c8542871608828294da9aadaea6c48e165
MOLLY-S1 <sankeym@middleton.school.nz>
2023-03-15 10:26:52 +1300

    Update Yahtzee_Lite_V1.py

--- Yahtzee_Lite_V1.py
+++ Yahtzee_Lite_V1.py
@@ -8,6 +8,7 @@ count = 1
 # numbers on dice
 NUMS = ["1","2","3","4","5","6"]
 
+
 # looping function to play again
 while True:
     play = easygui.buttonbox("Do you want to play Yahtzee! Lite", "Play",
@@ -17,8 +18,8 @@ while True:
         easygui.msgbox("Goodbye", "Goodbye Screen")
         break
 
-    
-    while count <= 3:
+
+    while count != 4:
         # list of rolls
         roll = []
 
@@ -28,10 +29,44 @@ while True:
             roll.append(dice)
 
         count += 1
-        sort_roll = roll.sort()
-        choice = easygui.buttonbox(f"You rolled {sort_roll}",f"Roll {count-1}",
+        roll.sort()
+
+
+
+        for item in roll:
+            show_roll = f",  ".join(roll)
+
+        choice = easygui.buttonbox(f"You rolled {show_roll}",f"Roll {count-1}",
                           choices= ["Roll Again", "Stick"])
+
         if choice == "Stick":
+            counter = 0
+            for option in roll:
+                repeats = roll.count(option)
+                if repeats > counter:
+                    counter=repeats
+
+            if counter == 5:
+                easygui.msgbox("Yahtzee!", "Score")
+                break
+            elif counter == 4:
+                easygui.msgbox("Four of a kind!", "Score")
+                break
+            elif counter == 3:
+                easygui.msgbox("Three of a kind!", "Score")
+                break
+            elif counter == 2:
+                easygui.msgbox("No matches made, better luck next time",
+                               "Score")
+                break
+            elif counter == 0:
+                easygui.msgbox("No matches made, better luck next time",
+                               "Score")
+                break
+    easygui.msgbox("Good Game!", "Game End")
+    count = 1
+
+
 
 
 
